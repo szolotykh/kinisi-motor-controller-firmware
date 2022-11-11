13170a57e28e8ecce4021207c65b89958633af27
Sergey Zolotykh <szolotykh88@gmail.com>
2022-11-10 22:34:51 -0800

    Added USB support

--- src/main.c
+++ src/main.c
@@ -1,42 +1,36 @@
 #include "stm32f4xx.h"
 #include "gpio.h"
 #include "motor.h"
+#include "usb_device.h"
 
-//TIM_HandleTypeDef htim12;
-//void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim);
-//static void MX_TIM12_Init(void);
+#define HAL_PCD_MODULE_ENABLED
+extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
 
 void SystemClock_Config(void);
+void OTG_FS_IRQHandler(void);
 
 int main(void)
 {
     HAL_Init();
     SystemClock_Config();
+    
+    // Init USB
+    MX_USB_DEVICE_Init();
 
+/*
     initialize_motor(0);
     set_motor_speed(0, 0, 840);
     HAL_Delay(2000);
     set_motor_speed(0, 1, 840);
     HAL_Delay(2000);
-
     set_motor_speed(0, 0, 0);
-
-    initialize_gpio();
- 
- /*
-    MX_TIM12_Init();
-    HAL_TIM_PWM_Start(&htim12, TIM_CHANNEL_1);
-    HAL_TIM_PWM_Start(&htim12, TIM_CHANNEL_2);
-    htim12.Instance->CCR1 = 840;
-		htim12.Instance->CCR2 = 0;
-    HAL_Delay(5000);
-    htim12.Instance->CCR1 = 0;
-		htim12.Instance->CCR2 = 0;
 */
-
+    HAL_Delay(2000);
     while (1){
-        gpio_toggle_status_led();
-        HAL_Delay(100);
+      	uint8_t *data = "Hello World! I am working!\n";
+	      CDC_Transmit_FS((uint8_t*) data, strlen(data));
+
+        HAL_Delay(1000);
     }
 }
 
@@ -93,20 +87,24 @@ void Error_Handler(void)
 
 void SystemClock_Config(void)
 {
+  // Endbling clock on port H since using cristal for HSE
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 8;
   RCC_OscInitStruct.PLL.PLLN = 168;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -124,68 +122,8 @@ void SystemClock_Config(void)
   }
 }
 
-/*
-static void MX_TIM12_Init(void)
+// Interrupt handler for USB
+void OTG_FS_IRQHandler(void)
 {
-  __HAL_RCC_TIM12_CLK_ENABLE();
-
-  TIM_OC_InitTypeDef sConfigOC = {0};
-
-  htim12.Instance = TIM12;
-  htim12.Init.Prescaler = 0;
-  htim12.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim12.Init.Period = 840-1;
-  htim12.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim12.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_PWM_Init(&htim12) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim12, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_TIM_PWM_ConfigChannel(&htim12, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  
-
-  HAL_TIM_MspPostInit(&htim12);
-
+  HAL_PCD_IRQHandler(&hpcd_USB_OTG_FS);
 }
-
-
-void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(htim->Instance==TIM12)
-  {
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-
-    GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    GPIO_InitStruct.Alternate = GPIO_AF9_TIM12;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-  }
-}
-
-
-void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef* htim_pwm)
-{
-  if(htim_pwm->Instance==TIM12)
-  {
-    // __HAL_RCC_TIM12_CLK_ENABLE();
-
-  }
-
-}
-
-*/
