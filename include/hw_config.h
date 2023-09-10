89376885bf1a91fad0b99b9a580868f55929d874
Sergey Zolotykh <szolotykh88@gmail.com>
2023-09-09 20:36:40 -0700

    Controller v03 (#6)
    * Add hardware configuration for v0.3 version of the controller
    * Updated GPIO mapping
    * Change motor pin mapping

--- include/hw_config.h
+++ include/hw_config.h
@@ -1,5 +1,5 @@
 //------------------------------------------------------------
-// File name: config.h
+// File name: hw_config.h
 //------------------------------------------------------------
 
 #pragma once
@@ -7,10 +7,125 @@
 #include "stm32f4xx_hal.h"
 #include "hw_config_structs.h"
 
-// Board version
-#define MP_V2
+// Board current version
+#define MP_V3
 
 // Boards configurations
+//------------------------------------------------------------
+#ifdef MP_V3
+	// Motor configurations
+	#define NUMBER_MOTORS 4
+
+	static const motor_info_t motor_info[NUMBER_MOTORS] = {
+		{
+			.timer = TIM12,
+			.pwmChannel1 = {
+							.timerChannel = TIM_CHANNEL_1,
+							.port = GPIOB,
+							.pin = GPIO_PIN_14,
+						   },
+			.pwmChannel2 = {
+							.timerChannel = TIM_CHANNEL_2,
+							.port = GPIOB,
+							.pin = GPIO_PIN_15,
+						   },
+		},
+		{
+			.timer = TIM8,
+			.pwmChannel1 = {
+							.timerChannel = TIM_CHANNEL_3,
+							.port = GPIOC,
+							.pin = GPIO_PIN_8,
+						   },
+			.pwmChannel2 = {
+							.timerChannel = TIM_CHANNEL_4,
+							.port = GPIOC,
+							.pin = GPIO_PIN_9,
+						   },
+		},
+		{
+			.timer = TIM8,
+			.pwmChannel1 = {
+							.timerChannel = TIM_CHANNEL_1,
+							.port = GPIOC,
+							.pin = GPIO_PIN_6,
+						   },
+			.pwmChannel2 = {
+							.timerChannel = TIM_CHANNEL_2,
+							.port = GPIOC,
+							.pin = GPIO_PIN_7,
+						   },
+		},
+		{
+			.timer = TIM1,
+			.pwmChannel1 = {
+							.timerChannel = TIM_CHANNEL_1,
+							.port = GPIOA,
+							.pin = GPIO_PIN_8,
+						   },
+			.pwmChannel2 = {
+							.timerChannel = TIM_CHANNEL_2,
+							.port = GPIOA,
+							.pin = GPIO_PIN_9,
+						   },
+		}
+	};
+
+	// Encoder configurations
+	#define NUMBER_ENCODERS 4
+
+	static const encoder_info_t encoder_info[NUMBER_ENCODERS] = {
+		{
+			.timer = TIM3,
+			.portA = GPIOA,
+			.pinA = GPIO_PIN_6,
+			.portB = GPIOA,
+			.pinB = GPIO_PIN_7
+		},
+		{
+			.timer = TIM5,
+			.portA = GPIOA,
+			.pinA = GPIO_PIN_0,
+			.portB = GPIOA,
+			.pinB = GPIO_PIN_1
+		},
+		{
+			.timer = TIM4,
+			.portA = GPIOB,
+			.pinA = GPIO_PIN_6,
+			.portB = GPIOB,
+			.pinB = GPIO_PIN_7
+		},
+		{
+			.timer = TIM2,
+			.portA = GPIOA,
+			.pinA = GPIO_PIN_15,
+			.portB = GPIOB,
+			.pinB = GPIO_PIN_3
+		}
+	};
+
+	// GPIO Configurations
+	#define NUMBER_GPIO_PINS 4
+
+	static gpio_info_t gpio_info[NUMBER_GPIO_PINS] = {
+		{.port = GPIOB, .pin = GPIO_PIN_13},
+		{.port = GPIOB, .pin = GPIO_PIN_12},
+		{.port = GPIOC, .pin = GPIO_PIN_10},
+		{.port = GPIOC, .pin = GPIO_PIN_11},
+		{.port = GPIOB, .pin = GPIO_PIN_5},
+		{.port = GPIOB, .pin = GPIO_PIN_4},
+		{.port = GPIOD, .pin = GPIO_PIN_2},
+		{.port = GPIOC, .pin = GPIO_PIN_12},
+		{.port = GPIOB, .pin = GPIO_PIN_9},
+		{.port = GPIOB, .pin = GPIO_PIN_8}
+	};
+
+	// Status pin configurations
+	#define STATUS_LED_PORT GPIOA
+	#define STATUS_LED_PIN GPIO_PIN_2
+
+#endif
 //------------------------------------------------------------
 #ifdef MP_V2
 	// Motor configurations
