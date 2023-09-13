484afa04919ae6e692132c225be11dabdacf6c1e
Sergey Zolotykh <szolotykh88@gmail.com>
2023-09-12 20:01:00 -0700

    Revert message length changes

--- tools/commands_generator/generator.py
+++ tools/commands_generator/generator.py
@@ -120,8 +120,8 @@ def generate_js_code(commands_data):
         for prop in cmd.get("properties", []):
             message_length += type_to_size_map.get(prop['type'], 1)  # Default to 1 byte if type is unknown
 
-        func_body += f"        const messageLength = {message_length + 1};\n"
-        func_body += f"        const buffer = new ArrayBuffer(messageLength);\n"
+        func_body += f"        const messageLength = {message_length};\n"
+        func_body += f"        const buffer = new ArrayBuffer(messageLength + 1);\n"
         func_body += f"        const view = new DataView(buffer);\n"
 
         func_body += f"        view.setUint8(0, messageLength);  // Message length\n"
